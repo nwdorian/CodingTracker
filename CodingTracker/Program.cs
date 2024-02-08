0d8217b056a52c245aecd5c3d7d0cab36e72530a
nwdorian <neuwirthdorian@gmail.com>
2024-02-08 20:45:28 +0100

    Add project files.

---
+++ CodingTracker/Program.cs
@@ -0,0 +1,3 @@
+﻿using CodingTracker;
+
+UserInput.MainMenu();
---
+++ CodingTracker/UserInput.cs
@@ -0,0 +1,28 @@
+﻿
+using Spectre.Console;
+using static CodingTracker.Models.Enums;
+
+namespace CodingTracker;
+
+internal class UserInput
+{
+    internal static void MainMenu()
+    {
+        bool repeatMenu = true;
+        while (repeatMenu)
+        {
+            repeatMenu = false;
+            Console.Clear();
+
+            var selection = AnsiConsole.Prompt(
+                new SelectionPrompt<MenuSelection>()
+                .Title("Welcome to [green]Coding tracker[/]\nWhat would you like to do?")
+                .PageSize(10)
+                .AddChoices(MenuSelection.ViewRecords,
+                MenuSelection.AddRecord,
+                MenuSelection.UpdateRecord,
+                MenuSelection.DeleteRecord)
+                );
+        }
+    }
+}
